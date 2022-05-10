b68afa6e248f19e48f415f5cff33f2445ffff294
weekbelt <vfrvfr4207@gmail.com>
2022-05-11 00:49:51 +0900

    Initial commit

---
+++ src/main/java/me/weekbelt/clientdirectory/ClientDirectoryApplication.java
@@ -0,0 +1,13 @@
+package me.weekbelt.clientdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientDirectoryApplication.class, args);
+    }
+
+}
